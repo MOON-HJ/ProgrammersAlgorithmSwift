9bfc17955378d958154246b30f1bb9b1a1c445e7
Moon HyoJae <followmoonrise@gmail.com>
2020-10-03 22:12:46 +0900

    FIX : Lv2 / Swift / ZipString
    Swift Level2 Printer.swift Completed

--- Level 2/ZipString.swift
+++ Level 2/ZipString.swift
@@ -1,48 +1,35 @@
-//2,5,7,17,18,20,21,23,27
-
 import Foundation
 
 func solution(_ s:String) -> Int {
-    let s:[String] = s.map{String($0)}
+    var s:[String] = Array(s.map{ String($0)})
+    let origin = s
     var answer = 1000
     
-    if s.count == 1 {
-        return 1
+    
+    if (0...1).contains(s.count) {
+        return s.count
     }
-    for x in 1...(s.count/2)+1 {
-        var string:[String] = []
-        var y = 0
-        var count=1
-        while y < s.count-1 {
-            if (y+x+x < s.count) && (s[y..<(y+x)] == s[(y+x)..<(y+x+x)]){
-                count+=1
-            } else if count<=1 {
-                if (y+x+x > s.count) {
-                    string.append(contentsOf: s[y...])
-                } else {
-                    string.append(contentsOf: Array(s[y..<(y+x)]))
-                }
-                count=1
+    
+    for n in 1...s.count/2 {
+        var line:[String] = []
+        var count = 1
+        while !s.isEmpty {
+            let prev = s.prefix(n)
+            s = Array(s.dropFirst(n))
+            if s.prefix(n) == prev {
+                count += 1
+            } else if count <= 1 {
+                line.append(contentsOf: Array(prev))
             } else {
-                string.append(String(count))
-                string.append(contentsOf: s[y..<(y+x)])
-                count=1
+                line.append(String(count))
+                line.append(contentsOf: Array(prev))
+                count = 1
             }
-            y += x
-            
         }
-        if string.reduce("", +).count < answer{
-            answer = string.reduce("", +).count
-        }
+        s = origin
+        let lineLenght = line.reduce("", +).count
+        answer = answer > lineLenght ? lineLenght : answer
     }
+    
     return answer
 }
-
-solution("aaaaaaaaaabbb") //5
-//solution("aaabbaccc") // 7
-//solution("")
-solution("ababcdcdababcdcd") // 9x
-//solution("abcabcdede") // 8
-solution("abcabcabcabcdededededede") //14x
-solution("xababcdcdababcdcd")// 17x
-solution("a")//x
