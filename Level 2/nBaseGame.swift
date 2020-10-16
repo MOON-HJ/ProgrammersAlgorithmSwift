f3fef329e0993412a362f242ef39ee2d79d42da6
Moon HyoJae <followmoonrise@gmail.com>
2020-10-16 12:39:27 +0900

    FIX : Lv2 / Swift / nBaseGame
    Swift Level2 nBaseGame.swift Completes

--- Level 2/nBaseGame.swift
+++ Level 2/nBaseGame.swift
@@ -1,13 +1,19 @@
-//14, 15, 16 시간초과
-
 func solution(_ n:Int, _ t:Int, _ m:Int, _ p:Int) -> String {
     var sequence = ""
+    var answer = ""
     var count = 0
-    while (sequence.count / m) <= t {
+    while true {
         sequence += String(count, radix: n)
         count += 1
+        if sequence.count >= m {
+            answer.append(Array(sequence)[p-1].uppercased())
+            sequence.removeFirst(m)
+        }
+        if answer.count == t {
+            return answer
+        }
     }
-    return String(sequence.enumerated().filter{$0.offset % m == p-1}.map{$1}.prefix(t)).uppercased()
+    return ""
 }
 
 solution(2, 4, 2, 1) // 0111
