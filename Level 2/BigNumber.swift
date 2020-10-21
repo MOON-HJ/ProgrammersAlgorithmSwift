7e100a831acdba004efda3538d5cd7b08f85250f
Moon HyoJae <followmoonrise@gmail.com>
2020-10-21 16:53:25 +0900

    FIX : Lv2 / Swift / BigNumber
    Complete

--- Level 2/BigNumber.swift
+++ Level 2/BigNumber.swift
@@ -1,24 +1,13 @@
-// 1~6, 11 failed
 import Foundation
 
 func solution(_ numbers:[Int]) -> String {
-    let numbers = numbers.map{String($0)}
-    let answer = numbers.sorted{compare($0, $1)}
-    return answer.reduce("", +)
-}
-
-func compare(_ a:String, _ b:String) -> Bool{
-    let prefixA = a.prefix(1)
-    let prefixB = b.prefix(1)
-    
-    func compareByPosition(_ a:String, _ b:String) -> Bool {
-        if a == "" || b == "" {
-            return (a == "" ? prefixA : a.prefix(1) ) >= (b == "" ? prefixB : b.prefix(1))
-        } else if a.prefix(1) == b.prefix(1) {
-            return compareByPosition(String(a.dropFirst()), String(b.dropFirst()))
-        } else {
-            return a.prefix(1) > b.prefix(1)
-        }
+    if numbers.filter({$0 != 0}).isEmpty {
+        return "0"
     }
-    return compareByPosition(a,b)
+    
+    let numbers = numbers.map{String($0)}
+    let answer = numbers.sorted{$0+$1 > $1+$0}
+    let result = answer.reduce("", +)
+    
+    return result
 }
