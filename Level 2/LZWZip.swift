27fecde64ad64ac3a7ba546aa87c52434c336b44
Moon HyoJae <followmoonrise@gmail.com>
2020-10-29 22:49:15 +0900

    fix( LZWZip ) : Lv2, LZWZip 문제해결
    LZWZip.swift

--- Level 2/LZWZip.swift
+++ Level 2/LZWZip.swift
@@ -2,32 +2,40 @@
 func solution(_ msg:String) -> [Int] {
     var dics:Dictionary<String,Int> = [:]
     var count:Int = 1
+    var msg:[String] = Array(msg).map{String($0)}
+    var answer:[Int] = []
     (65...90).map({Character(UnicodeScalar($0))}).forEach{ i in
         dics[String(i)] = count
         count += 1
     }
+    if msg.count == 1 {
+        return [dics[msg[0]]!]
+    }
     
-    var answer:[Int] = []
-    var msg = Array(msg)
-    var curr:String = String(msg[0])
-    var next:String = String(msg[1])
-    while msg.count != 1 {
-        if dics[curr+next] != nil {
-            answer.append(dics[curr+next]!)
-        }else {
-            dics[curr+next] = count
-            count += 1
+    var max = 1
+    var w = msg[0]
+    var c = msg[1]
+    while msg.count >= 1  {
+        print("(w:\(w), c:\(c))")
+        answer.append(dics[w]!)
+        dics[w+c] = count
+        count += 1
+        msg.removeFirst(w.count)
+        
+        // next w
+        max = max>(w+c).count ? max : (w+c).count
+        for i in stride(from: max, to: 1, by: -1) {
+            let nextw:String = Array(msg.prefix(i)).reduce("", +)
+            if dics[nextw] != nil {
+                w = nextw
+                c = msg.dropFirst(i).first ?? ""
+                break
+            } else {
+                w = msg.first ?? ""
+                c = msg.dropFirst().first ?? ""
+            }
         }
-            
-        dics[curr]
-        answer.append(dics[curr] ?? 0)
-        msg.removeFirst(1)
     }
     
     return answer
 }
-
-
-//  solution("KAKAO") // [11, 1, 27, 15]
-solution("TOBEORNOTTOBEORTOBEORNOT") // [20, 15, 2, 5, 15, 18, 14, 15, 20, 27, 29, 31, 36, 30, 32, 34]
-
