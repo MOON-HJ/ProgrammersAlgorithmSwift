f06d7ed70d055af4c10e90ba481df91a2bd5e765
Moon HyoJae <followmoonrise@gmail.com>
2021-01-26 19:35:40 +0900

    fix : ( TargetNumber ) : Lv2, TargetNumber
    테스트 1, 2 시간 초과 / MAX 값 구하기 최적화

--- Level 2/TargetNumber.swift
+++ Level 2/TargetNumber.swift
@@ -2,9 +2,9 @@ import Foundation
 
 
 func solution(_ numbers:[Int], _ target:Int) -> Int {
-    let totalCaseCount:Int = Int(truncating: NSDecimalNumber(decimal: pow(2, numbers.count)))
+    let max:Int = 1 << numbers.count
     var answer = 0
-    (0..<totalCaseCount).forEach { i in
+    (0..<max).forEach { i in
         let b = String(i, radix: 2).map{$0 == "1"}
         let cases = [Bool](repeating: false, count: numbers.count-b.count) + b
         let sum = zip(numbers, cases).map{$1 ? $0 : -$0}.reduce(0, +)
