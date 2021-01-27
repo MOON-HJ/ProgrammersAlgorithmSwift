6e3f8809575cf3e9332d967cb425fa3c1a4a2bc6
Moon HyoJae <followmoonrise@gmail.com>
2021-01-27 13:29:38 +0900

    fix : ( TargetNumber ) : Lv2, TargetNumber complete

--- Level 2/TargetNumber.swift
+++ Level 2/TargetNumber.swift
@@ -4,11 +4,13 @@ import Foundation
 func solution(_ numbers:[Int], _ target:Int) -> Int {
     let max:Int = 1 << numbers.count
     var answer = 0
-    (0..<max).forEach { i in
-        let b = String(i, radix: 2).map{$0 == "1"}
-        let cases = [Bool](repeating: false, count: numbers.count-b.count) + b
-        let sum = zip(numbers, cases).map{$1 ? $0 : -$0}.reduce(0, +)
-        answer += (sum == target) ? 1 : 0
+
+    (0..<max).forEach { cur in
+        var count = 0
+        for i in  0..<numbers.count {
+            count += (((cur >> i) & 1) == 1 ? 1 : -1) * numbers[i]
+        }
+        answer += ( count == target ? 1 : 0)
     }
 
     return answer
