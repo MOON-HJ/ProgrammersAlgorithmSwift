d5e24778a8263977818299969ae69a3c8f805a12
Moon HyoJae <followmoonrise@gmail.com>
2021-02-24 10:43:56 +0900

    fix : ( NextBigNumber )  Lv2, NextBigNumber Improve

--- Level 2/NextBigNumber.swift
+++ Level 2/NextBigNumber.swift
@@ -1,13 +1,10 @@
 import Foundation
 
 func solution(_ n:Int) -> Int {
-  var answer = n
-    let count = Array(String(n, radix: 2)).filter{$0=="1"}.count
-    
+    var answer = n    
     repeat {
         answer += 1
-        
-    } while Array(String(answer, radix: 2)).filter{$0=="1"}.count != count
+    } while answer.nonzeroBitCount != n.nonzeroBitCount
     return answer
-    
 }
+
