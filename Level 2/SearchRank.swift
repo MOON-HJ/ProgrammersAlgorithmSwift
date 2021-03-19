a456631c791b8d8a6c9913aceb7c0f53e6ed7f3c
Moon HyoJae <followmoonrise@gmail.com>
2021-03-19 17:55:40 +0900

    feat : ( SearchRank )  Lv2, SearchRank
    효율성 테스트 시간초과

--- Level 2/SearchRank.swift
+++ Level 2/SearchRank.swift
@@ -1,33 +1,15 @@
 import Foundation
 
-struct Recruit {
-    var language: String
-    var position: String
-    var career: String
-    var food: String
-    var score: Int
-}
-
-
 func solution(_ info:[String], _ query:[String]) -> [Int] {
-    let _info:[Recruit] = info.map{
-        let data = $0.split(separator: " ")
-        return Recruit(language: String(data[0]), position: String(data[1]), career: String(data[2]), food: String(data[3]), score: Int(data[4])!)
-    }
-    let _query:[Recruit] = query.map{
-        let data = $0.components(separatedBy: " ").filter{$0 != "and"}
-        return Recruit(language: String(data[0]), position: String(data[1]), career: String(data[2]), food: String(data[3]), score: Int(data[4])!
-        )
-    }
-    return _query.map{ q in
-        _info.filter{ i in 
-            return (i.language == q.language || q.language == "-") && (i.position == q.position || q.position == "-") &&
-                (i.career == q.career || q.career == "-") &&
-            (i.food == q.food || q.food == "-") &&
-                (i.score >= q.score)
-        }.count
+    return query.map{
+        let data = $0.components(separatedBy: " ")
+        return info.filter{  
+            let i = $0.components(separatedBy: " ")
+            return (i[0] == String(data[0]) || String(data[0]) == "-") && 
+                (i[1] == String(data[2]) || String(data[2]) == "-") &&
+                (i[2] == String(data[4]) || String(data[4]) == "-") &&
+                (i[3] == String(data[6]) || String(data[6]) == "-") &&
+                (Int(i[4])! >= Int(data[7])!)}.count
     }
 }
-
-
 
